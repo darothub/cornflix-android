772f903a007d8efc7bf284e80e0ba89c72c7c9d9
lepea <darotudeen@gmail.com>
2020-02-10 15:19:18 +0100

    starter files

---
+++ app/src/main/java/com/darotapp/cornflix/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.darotapp.cornflix.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.darotapp.cornflix.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
